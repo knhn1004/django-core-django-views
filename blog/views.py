44191a70a35d4e65d3f0fbb349b5e9ef1913fd8f
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-22 12:43:55 +0800

    list search query

--- blog/views.py
+++ blog/views.py
@@ -48,38 +48,23 @@ def post_model_robust_view(req, id=None):
 #@login_required(login_url='/login/')
 def post_model_list_view(req):
     qs = PostModel.objects.all()
+    #print(qs)
 
-    print(qs)
-    #return HttpResponse('some data')
+    #print(req.GET)  # query dict
+    #query = req.GET['q']
+    query = req.GET.get('q')
+
+    if query:
+        qs = qs.filter(title__icontains=query)
+
     template = "list-view.html"
     context = {"object_list": qs}
-
-    #if (req.user.is_authenticated):
-    #    print('logged in')
-    #    template = "list-view.html"
-    #else:
-    #    print('not logged in')
-    #    #template_path = "list-view-public.html"
-    #    return HttpResponseRedirect("/login")
-    #    #raise Http404
 
     return render(req, template, context)
 
 
 def post_model_detail_view(req, id=None):
     print("keyword: ", id)
-    #obj = PostModel.objects.get(id=1)
-
-    #try:
-    #    obj = PostModel.objects.get(id=100)
-    #except:
-    #    raise Http404
-
-    #qs = PostModel.objects.filter(id=100)
-    #if not qs.exists() and qs.count() != 1:
-    #    raise Http404
-    #else:
-    #    obj = qs.first()
 
     obj = get_object_or_404(PostModel, id=id)
     context = {"object": obj}
@@ -89,13 +74,6 @@ def post_model_detail_view(req, id=None):
 
 
 def post_model_create_view(req):
-
-    #if req.method == 'POST':
-    #    print(req.POST)
-    #    form = PostModelForm(req.POST)
-    #    if form.is_valid():
-    #        form.save(commit=False)
-    #        print(form.cleaned_data)
 
     form = PostModelForm(req.POST or None)
 
